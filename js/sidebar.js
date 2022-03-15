94b1bf9c13442a9dce3975bf53174a9fd07b6779
RafaHBpt <rafaefx@gmail.com>
2022-03-14 18:57:22 -0600

    Agregar primera versión footer, mejorar sidebar

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,7 +1,7 @@
 document.getElementsByClassName("openbtn")[0].addEventListener("click", function() {
     document.getElementById("mySidebar").style.width = "250px";
     document.getElementById("mySidebar").style.zIndex = 3;
-    document.getElementById("menu").style.overflow = "hidden";
+    document.querySelector("body").style.overflowY = "hidden";
     document.getElementsByClassName("openbtn")[0].style.opacity = "0";
     document.getElementById("menu").style.opacity = "0.8";
 });
@@ -11,5 +11,5 @@ document.getElementsByClassName("closebtn")[0].addEventListener("click", functio
     document.getElementById("mySidebar").style.zIndex = 1;
     document.getElementsByClassName("openbtn")[0].style.opacity = "1";
     document.getElementById("menu").style.opacity = "1";
-    document.getElementById("menu").style.overflow = "visible";
+    document.querySelector("body").style.overflowY = "visible";
 });
